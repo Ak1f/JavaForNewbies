6a4645cdc33187123747e47c8aacdfbe93534bf8
Dennis <akifiction@gmail.com>
2016-06-05 21:21:58 +0300

    Update AdditionOp.java

--- AdditionOp.java
+++ AdditionOp.java
@@ -1,9 +1,8 @@
 //The Addition Operations
 
-
 public class AdditionOp {
     public static void main(String[] args) {
-        	System.out.print("244 + 835 = ");
-	System.out.print(244 + 835);
+        	System.out.print("275 + 835 = ");
+	System.out.print(275 + 835);
     }
 }
