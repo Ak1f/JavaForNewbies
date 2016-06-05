8ad4c4f165f5db4a79ac22d4bb59a6fba03af4fe
Dennis <akifiction@gmail.com>
2016-06-05 21:27:20 +0300

    Update AdditionOp.java

--- AdditionOp.java
+++ AdditionOp.java
@@ -1,8 +1,27 @@
 //The Addition Operations
 
+//The comments are shown in Java as
+/* text */	//The compiler ignores everything from /* to */.
+// text	The compiler ignores everything from // to the end of the line.
+/** documentation */	
+
+
+
+
+
 public class AdditionOp {
     public static void main(String[] args) {
         	System.out.print("275 + 835 = ");
 	System.out.print(275 + 835);
+	
+//	it coult be made such 
+	
+	int a=275;
+	int b=835;
+	int total=a+b;
+	
+	System.out.print(total);	
+	
+	
     }
 }
