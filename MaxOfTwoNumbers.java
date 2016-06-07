8da264376e59c1b74239e93a03d26cc443272857
Akif <akifiction@gmail.com>
2016-06-07 18:53:47 +0300

    Create MaxOfTwoNumbers.java

---
+++ MaxOfTwoNumbers.java
@@ -0,0 +1,21 @@
+
+public class MaxOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        /*
+         * To find maximum of two int values, use
+         * static int max(int a, int b) method of Math class.
+         */
+        System.out.println(Math.max(20, 40));
+
+        System.out.println("\n");
+        System.out.println(Math.max(324.34f, 432.324f));
+
+        System.out.println("\n");
+        System.out.println(Math.max(65.34, 123.45));
+        System.out.println("\n");
+
+        System.out.println(Math.max(435l, 523l));
+    }
+}
